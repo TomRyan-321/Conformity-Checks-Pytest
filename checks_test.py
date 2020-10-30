1de712624518cb822acc9e9090db594107327248
TomRyan-321 <25472582+TomRyan-321@users.noreply.github.com>
2020-10-30 19:48:27 +1100

    add most filters

--- checks_test.py
+++ checks_test.py
@@ -1,31 +1,49 @@
-import pytest
 import requests
 import os
 
-# CC Region  + API Key
+# Conformity Region, API Key & Target Account(s) variables
 CC_REGION = os.environ.get("CC_REGION", "us-west-2")
-CC_APIKEY = os.environ.get("CC_APIKEY")
+CC_APIKEY = os.environ["CC_APIKEY"]
+CC_ACCOUNTIDS = os.environ["CC_ACCOUNTIDS"]
 
 # Checks API Filters
-CC_ACCOUNTIDS = os.environ.get("CC_ACCOUNTIDS")
-CC_RISK_LEVELS = os.environ.get("CC_RISKLEVELS", "EXTREME,VERY_HIGH,HIGH,MEDIUM,LOW")
+CC_FILTER_CATEGORIES = os.environ.get("CC_FILTER_CATEGORIES", "")
+CC_FILTER_COMPLIANCES = os.environ.get("CC_FILTER_COMPLIANCES", "")
+CC_FILTER_CREATEDLESSTHAN = os.environ.get("CC_FILTER_CREATEDLESSTHAN", "1")
+CC_FILTER_REGIONS = os.environ.get("CC_FILTER_REGIONS", "")
+CC_FILTER_RISKLEVELS = os.environ.get("CC_FILTER_RISKLEVELS", "")
+CC_FILTER_SERVICES = os.environ.get("CC_FILTER_SERVICES", "")
+CC_FILTER_TAGS = os.environ.get("CC_FILTER_TAGS", "")
 
-# Limits_allowed_before_failing_tests
+# Limits allowed before failing tests
 MAX_TOTAL = int(os.environ.get("MAX_TOTAL", 0))
 MAX_EXTREME = int(os.environ.get("MAX_EXTREME", 0))
 MAX_VERY_HIGH = int(os.environ.get("MAX_VERY_HIGH", 0))
 MAX_HIGH = int(os.environ.get("MAX_HIGH", 0))
-MAX_MEDIUM = int(os.environ.get("MEDIUM", 0))
-MAX_LOW = int(os.environ.get("MEDIUM", 0))
+MAX_MEDIUM = int(os.environ.get("MAX_MEDIUM", 0))
+MAX_LOW = int(os.environ.get("MAX_LOW", 0))
 
 url = (
     "https://"
     + CC_REGION
     + "-api.cloudconformity.com/v1/checks?accountIds="
     + CC_ACCOUNTIDS
+    + "&filter[categories]="
+    + CC_FILTER_CATEGORIES
+    + "&filter[compliances]="
+    + CC_FILTER_COMPLIANCES
+    + "&filter[createdLessThanDays]="
+    + CC_FILTER_CREATEDLESSTHAN
+    + "&filter[regions]="
+    + CC_FILTER_REGIONS
     + "&filter[riskLevels]="
-    + CC_RISK_LEVELS
-    + "&page[size]=1000&filter[statuses]=FAILURE"
+    + CC_FILTER_RISKLEVELS
+    + "&filter[services]="
+    + CC_FILTER_SERVICES
+    + "&filter[tags]="
+    + CC_FILTER_TAGS
+    + "&filter[statuses]=FAILURE"
+    + "&page[size]=1000"
 )
 
 payload = {}
@@ -46,47 +64,35 @@ def test_total_failures_exceed_limit():
 
 
 def test_extreme_failures_exceed_limit():
-    assert (
-        sum(
-            1
-            for c in response_json["data"]
-            if c["attributes"]["risk-level"] == "EXTREME"
-        )
-        <= MAX_EXTREME
+    count = sum(
+        1 for c in response_json["data"] if c["attributes"]["risk-level"] == "EXTREME"
     )
+    assert count <= MAX_EXTREME
 
 
 def test_very_high_failures_exceed_limit():
-    assert (
-        sum(
-            1
-            for c in response_json["data"]
-            if c["attributes"]["risk-level"] == "VERY_HIGH"
-        )
-        <= MAX_VERY_HIGH
+    count = sum(
+        1 for c in response_json["data"] if c["attributes"]["risk-level"] == "VERY_HIGH"
     )
+    assert count <= MAX_VERY_HIGH
 
 
 def test_high_failures_exceed_limit():
-    assert (
-        sum(1 for c in response_json["data"] if c["attributes"]["risk-level"] == "HIGH")
-        <= MAX_HIGH
+    count = sum(
+        1 for c in response_json["data"] if c["attributes"]["risk-level"] == "HIGH"
     )
+    assert count <= MAX_HIGH
 
 
 def test_medium_failures_exceed_limit():
-    assert (
-        sum(
-            1
-            for c in response_json["data"]
-            if c["attributes"]["risk-level"] == "MEDIUM"
-        )
-        <= MAX_MEDIUM
+    count = sum(
+        1 for c in response_json["data"] if c["attributes"]["risk-level"] == "MEDIUM"
     )
+    assert count <= MAX_MEDIUM
 
 
 def test_low_failures_exceed_limit():
-    assert (
-        sum(1 for c in response_json["data"] if c["attributes"]["risk-level"] == "LOW")
-        <= MAX_LOW
+    count = sum(
+        1 for c in response_json["data"] if c["attributes"]["risk-level"] == "LOW"
     )
+    assert count <= MAX_LOW
